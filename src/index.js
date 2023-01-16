934df39325794cf554b71267047870f2b0549f1e
douglas godois dos santos <doug_potter92@hotmail.com>
2023-01-15 21:50:52 -0300

    teste1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/App';
+import { HashRouter } from "react-router-dom";
+
 
 
 import GlobalStyles from './global';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <GlobalStyles />
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
