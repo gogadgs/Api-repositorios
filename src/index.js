b0ea1248a0c0eee81c6e876984932ecf26b0e464
DOUGLAS GODOIS DOS SANTOS <86927046+gogadgs@users.noreply.github.com>
2023-02-02 12:24:41 -0300

    atualizando

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/App';
-import { HashRouter } from "react-router-dom";
+
 
 
 
@@ -9,8 +9,8 @@ import GlobalStyles from './global';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter base='/'>
+  <React.StrictMode>
     <GlobalStyles />
     <App />
-  </HashRouter>
+    </React.StrictMode>
 );
